76a1c845250a5c7d014fc5af2590918642b6bdce
Mady Chapman <MC3D@users.noreply.github.com>
2019-10-18 11:41:13 -0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic.edit import CreateView
 
+from .forms import CustomUserCreationForm
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name= 'signup.html'
+    template_name = 'signup.html'
